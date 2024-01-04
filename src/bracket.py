9a904c374ff84865b87ba8636713b1b9dd28fbe6
Matthew Urffer <matthew.urffer@gmail.com>
2024-01-04 01:32:42 +0000

    cleaning up the formats on save

--- src/bracket.py
+++ src/bracket.py
@@ -10,6 +10,7 @@ class Team:
     A team is a set of players
     >>> a = Team({0,1,2,3})
     """
+
     def __init__(self, players):
         self._players = frozenset(players)
 
@@ -19,7 +20,7 @@ class Team:
 
     def __hash__(self):
         return hash(self.players)
- 
+
     def __eq__(self, other):
         # Equaility is based upon the players, not the order
         if isinstance(other, self.__class__):
@@ -28,8 +29,7 @@ class Team:
 
 
 class Matchup:
-    """Matchup between two teams
-    """
+    """Matchup between two teams"""
 
     def __init__(self, a: Team, b: Team):
         self._teams = [a, b]
@@ -64,8 +64,7 @@ class Matchup:
 
 
 class Round:
-    """Matchups of a given round
-    """
+    """Matchups of a given round"""
 
     def __init__(self, matchups: list[Matchup]):
         self._matchups = matchups
@@ -80,9 +79,7 @@ class Round:
 
 
 def get_teams(round_i):
-    """
-    
-    """
+    """ """
     # All of the teams in a round
     teams_i = [team_i for team in round_i for team_i in team]
     return teams_i
--- test.py
+++ test.py
@@ -1,54 +1,55 @@
 import unittest
 from src.bracket import Team, Matchup, Round, unique_matchup, unique_teams
 
+
 class TestTeam(unittest.TestCase):
-    
     def test_team_construct(self):
-        t = Team({0,1,2,3})
-        self.assertSetEqual(t.players, {0,1,2,3})
+        t = Team({0, 1, 2, 3})
+        self.assertSetEqual(t.players, {0, 1, 2, 3})
 
     def test_team_equality(self):
-        self.assertTrue(Team({1,2}) == Team({2,1}))
-        self.assertFalse(Team({1,2}) == Team({2,3}))
+        self.assertTrue(Team({1, 2}) == Team({2, 1}))
+        self.assertFalse(Team({1, 2}) == Team({2, 3}))
 
     def test_team_hash(self):
-        self.assertTrue(hash(Team({1,2})) == hash(Team({2,1})))
-        self.assertFalse(hash(Team({1,2})) == hash(Team({2,3})))
+        self.assertTrue(hash(Team({1, 2})) == hash(Team({2, 1})))
+        self.assertFalse(hash(Team({1, 2})) == hash(Team({2, 3})))
+
 
 class TestMatchup(unittest.TestCase):
-
     def test_matchup_construct(self):
-        a = {0,1,2}
-        b = {3,4,5}
+        a = {0, 1, 2}
+        b = {3, 4, 5}
         m = Matchup(Team(a), Team(b))
         self.assertListEqual(m.get_players(), list(a | b))
 
     def test_unique_players(self):
-        a = {0,1,2}
-        b = {3,4,5}
+        a = {0, 1, 2}
+        b = {3, 4, 5}
         m = Matchup(Team(a), Team(b))
         self.assertTrue(m.unique_players())
 
         m = Matchup(Team(a), Team(a))
         self.assertFalse(m.unique_players())
 
-class TestRound(unittest.TestCase):
 
-    a = {0,1}
-    b = {2,3}
-    c = {4,5}
-    d = {6,7}
+class TestRound(unittest.TestCase):
+    a = {0, 1}
+    b = {2, 3}
+    c = {4, 5}
+    d = {6, 7}
     m1 = Matchup(Team(a), Team(b))
     m2 = Matchup(Team(c), Team(d))
 
     def test_round_construct(self):
-
         r = Round([self.m1, self.m2])
         self.assertEqual(r.matchups, [self.m1, self.m2])
 
     def test_get_teams(self):
         r = Round([self.m1, self.m2])
-        self.assertListEqual(r.get_teams(), [Team(self.a), Team(self.b), Team(self.c), Team(self.d)])
+        self.assertListEqual(
+            r.get_teams(), [Team(self.a), Team(self.b), Team(self.c), Team(self.d)]
+        )
 
     def test_get_players(self):
         # elf.a | self.b | self.c | self.d
@@ -56,19 +57,16 @@ class TestRound(unittest.TestCase):
 
 
 class TestRules(unittest.TestCase):
-
     def test_unique_matchup(self):
-
         u0 = ((0, 1), (2, 3))
         self.assertTrue(unique_matchup(u0))
 
         u1 = ((0, 1), (0, 2))
         self.assertFalse(unique_matchup(u1))
 
-
     def test_unqiue_teams(self):
         pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
